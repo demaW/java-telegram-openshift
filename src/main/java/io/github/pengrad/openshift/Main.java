f83dd288fbd683c5649ae342da000b232601d155
dema_w <demavov@gmail.com>
2016-05-08 16:00:25 +0300

    added hello function

--- src/main/java/io/github/pengrad/openshift/Main.java
+++ src/main/java/io/github/pengrad/openshift/Main.java
@@ -13,18 +13,21 @@ public class Main {
     private static Logger log = Logger.getLogger(Main.class.getName());
 
     private static final File FAVICON_PATH = new File("src/main/resources/favicon.ico");
+    static TestTelegramBot bot;
 
     public static void main(String[] args) {
         ipAddress(args[0]);
         port(Integer.parseInt(args[1]));
 
-        log.info("ip is: " + args[0]);
+        log.info("ip is: " + args[0] + ":" + args[1]);
 
         get("/", (request, response) -> {
             response.status(200);
             log.info("in /");
             return "";
         });
+
+        get("/help", (request, response) -> "this is help command");
 
         get("/favicon.ico", (request, response) -> {
                     response.status(200);
@@ -33,10 +36,13 @@ public class Main {
         );
 
         // Bot handler
-        post("/myBot", new TestTelegramBot());
+        post("/myBot", bot = new TestTelegramBot());
 
         // simple string by GET
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> {
+            System.out.println(bot.bot.getMe().isOk());
+            return "Hello World";
+        });
 
         // GET/POST handler
         post("/test", new Test());
